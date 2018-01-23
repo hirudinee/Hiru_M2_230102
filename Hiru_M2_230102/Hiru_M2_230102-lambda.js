d7c5795b583769b57b5a6aedb54d6250315e5c87
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-01-23 14:22:23 +0530

    S3 copy

--- Hiru_M2_230102/Hiru_M2_230102-lambda.js
+++ Hiru_M2_230102/Hiru_M2_230102-lambda.js
@@ -1,6 +1,29 @@
 let AWS = require('aws-sdk');
-exports.handler = function(event, context, callback) {
+const s3 = new AWS.S3();
+const sns = new AWS.SNS();
+const ddb = new AWS.DynamoDB.DocumentClient();
+exports.handler = function (event, context, callback) {
+
+	s3.copyObject({
+		'Bucket': "hiru.sampleresized",
+		'CopySource': "/hiru.sample/",
+		'Key': ""
+	}).promise()
+		.then(data => {
+			console.log(data);           // successful response
+			/*
+			data = {
+				CopyObjectResult: {
+					ETag: "\"6805f2cfc46c0f04559748bb039d69ae\"",
+					LastModified: <Date Representation>
+				}
+			}
+			*/
+		})
+		.catch(err => {
+			console.log(err, err.stack); // an error occurred
+		});
 
 
-	callback(null,'Successfully executed');
+	callback(null, 'Successfully executed');
 }
